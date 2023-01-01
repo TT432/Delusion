776c2db88524c658a37aa489cc2a1f05df319b1b
TT432 <2437315224@qq.com>
2023-01-01 14:56:14 +0800

    Initial commit

---
+++ src/main/java/io/github/tt432/delusion/Delusion.java
@@ -0,0 +1,16 @@
+package io.github.tt432.delusion;
+
+import net.minecraftforge.fml.common.Mod;
+
+/**
+ * @author DustW
+ */
+@Mod(Delusion.MOD_ID)
+public class Delusion {
+
+    public static final String MOD_ID = "delusion";
+
+    public Delusion() {
+        
+    }
+}
